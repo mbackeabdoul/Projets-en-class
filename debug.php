2e9baa828b656ac5c2a0af8213b269190085e15a
Abdou Khoudoss Mbacke <khoudossmbacke18gmail.com>
2026-08-18 15:06:56 +0000

    feat(Model + Test ): Creation de la classe Database + les Repository + la classe Debug

--- debug.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function dd(mixed $value): void
-{
-    echo '<pre style="background:#111;color:#fff;padding:12px;border-radius:8px;overflow:auto;">';
-    var_dump($value);
-    echo '</pre>';
-    die();
-}
